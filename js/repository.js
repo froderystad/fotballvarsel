940c95039e644577b24d480245b384d471871b7b
Frode Rystad <frode-git@rystad.no>
2015-11-06 08:57:14 +0100

    Refaktorert repository.js

--- js/repository.js
+++ js/repository.js
@@ -14,34 +14,24 @@ exports.findAllTeams = function(successCallback) {
 };
 
 exports.replaceTeams = function(teams, successCallback) {
-  mongoClient.connect(url, function(err, db) {
-    if (err) console.log("Error: " + err);
-    var collection = db.collection("teams");
-    collection.deleteMany({}, function(error, result) {
-      if (error) console.log("Error: " + error);
-      console.log("Deleted " + result.deletedCount + " team entries");
-
-      collection.insertMany(teams, function(error, result) {
-        if (error) console.log("Error: " + error);
-        console.log("Inserted " + result.insertedCount + " team configurations");
-        db.close();
-        successCallback(result.ops);
-      });
-    });
-  });
+  deleteAndInsertMany("teams", teams, successCallback);
 };
 
 exports.replaceSubscribers = function(subscribers, successCallback) {
+  deleteAndInsertMany("subscribers", subscribers, successCallback);
+};
+
+var deleteAndInsertMany = function(collectionName, items, successCallback) {
   mongoClient.connect(url, function(error, db) {
     if (error) console.log("connect error: " + error);
-    var collection = db.collection("subscribers");
+    var collection = db.collection(collectionName);
     collection.deleteMany({}, function(error, result) {
       if (error) console.log("deleteMany error: " + error);
-      collection.insertMany(subscribers, function(error, result) {
+      collection.insertMany(items, function(error, result) {
         if (error) console.log("insertMany error: " + error);
         db.close();
         successCallback(result.ops);
       });
     });
   });
-};
+}
