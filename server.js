d29120a8402407074b8a68102e21edb385261cfc
Frode Rystad <frode-git@rystad.no>
2015-11-06 08:06:55 +0100

    Fungerende GET for team

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var repository = require('./js/repository.js');
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000;
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.json({message: 'Velkommen til Fotballvarsel for Skeid.no' });
+});
+
+router.route('/teams')
+    .get(function(req, res) {
+      repository.findAllTeams(function(teams) {
+        res.json(teams);
+      });
+    });
+
+app.use('/api', router)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Velkommen til Fotballvarsel for Skeid.no');
 });
 
-var server = app.listen(app.get('port'), function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Fotballvarsel listening at http://%s:%s', host, port);
-});
+app.listen(port);
+console.log('Fotballvarsel listening on port %s', port);
