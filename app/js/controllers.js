6548b6734d710b4a51f1900de0c061ac318cb284
Frode Rystad <frode-git@rystad.no>
2015-11-13 14:08:45 +0100

    Introduced Subscriber service to hold state between controllers, and Subscribers service is now a resource.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,19 +1,23 @@
 var controllers = angular.module('controllers', []);
 
-controllers.controller('LoginCtrl', ['$scope', '$location', 'Email', 'Subscriber', function ($scope, $location, Email, Subscriber) {
+controllers.controller('LoginCtrl', ['$scope', '$location', 'Email', 'Subscribers', 'Subscriber', function ($scope, $location, Email, Subscribers, Subscriber) {
     $scope.email = Email;
 
     $scope.login = function() {
-        console.log("%s logged in", $scope.email.email);
-        console.log("Subscriber is %s", Subscriber.email);
-        $location.path('/subscriber');
+        var email = $scope.email.email;
+        console.log("%s logged in", email);
+        Subscribers.get({email: email}, function(subscriber) {
+            Subscriber.theSubscriber = subscriber;
+            console.log("Subscriber is %s", subscriber.email);
+            $location.path('/subscriber');
+        });
     };
 }]);
 
 controllers.controller('SubscriberCtrl', ['$scope', 'Subscriber', 'Teams', function ($scope, Subscriber, Teams) {
-    $scope.subscriber = Subscriber;
+    $scope.subscriber = Subscriber.theSubscriber;
     $scope.teams = Teams.query();
-    console.log("Subscriber is %s", Subscriber.email);
+    console.log("Subscriber is %s", $scope.subscriber);
 }]);
 
 
--- app/js/services.js
+++ app/js/services.js
@@ -4,16 +4,13 @@ services.factory('Email', function() {
     return { email: null };
 });
 
-services.factory('Subscriber', function(){
-    return {
-        "_id": "563f8556622a4f0c64b28cbb",
-        "email": "frode.rystad@gmail.com",
-        "teams": [
-            "G2009",
-            "G2007"
-        ]
-    };
+services.factory('Subscriber', function() {
+    return { theSubscriber: null };
 });
+
+services.factory('Subscribers', ['$resource', function($resource){
+    return $resource('/api/subscribers/:email');
+}]);
 
 services.factory('Teams', ['$resource', function($resource){
     return $resource('/api/teams', null);
