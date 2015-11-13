8c75fb09aec631b48ed2030e146e6d23732f4b22
Frode Rystad <frode-git@rystad.no>
2015-11-13 13:35:21 +0100

    Moved services to separate js-file.

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'ngResource', 'controllers']);
-
-app.factory('Email', function() {
-    return { email: null };
-});
-
-app.factory('Subscriber', function(){
-    return {
-            "_id": "563f8556622a4f0c64b28cbb",
-            "email": "frode.rystad@gmail.com",
-            "teams": [
-                "G2009",
-                "G2007"
-            ]
-        };
-});
-
-app.factory('Teams', ['$resource', function($resource){
-    return $resource('/api/teams', null);
-}]);
+var app = angular.module('app', ['ngRoute', 'ngResource', 'controllers', 'services']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
