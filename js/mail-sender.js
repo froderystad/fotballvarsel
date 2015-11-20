bf0319019342f76d62a479e86d1e246cb16b873d
Frode Rystad <frode-git@rystad.no>
2015-11-20 09:52:49 +0100

    Notification e-mail now contains link to manage subscriptions.

--- js/mail-sender.js
+++ js/mail-sender.js
@@ -5,14 +5,15 @@ var hostnameWithPort = process.env.TEST_HOSTNAME_WITH_PORT || "fotballvarsel.her
 
 var client = new postmark.Client(process.env.POSTMARK_API_KEY);
 var fromAddress = "fotballvarsel@rystad.no";
-var footer = "Takk for at du bruker Fotballvarsel-tjenesten! Feil og forbedringsønsker kan meldes til fotballvarsel@rystad.no.";
+var footer = "Takk for at du bruker Fotballvarsel! Feil og forbedringsønsker kan meldes til fotballvarsel@rystad.no.";
 
 exports.sendEmail = function(subscriber, team, newArticles) {
     client.sendEmail({
       "From": fromAddress,
       "To": subscriber.email,
       "Subject": team.name + " har " + newArticles.length + " nye artikler", 
-      "TextBody": "Disse artiklene er nye:\n\n" + renderArticleUrls(newArticles) + footer
+      "TextBody": "Disse artiklene er nye:\n\n" + renderArticleUrls(newArticles) +
+          footer + "\n\nLink for å endre dine abonnementer:\n" + loginLink(subscriber)
   });
 };
 
@@ -31,7 +32,11 @@ exports.newSecret = function(subscriber, callback) {
         "To": subscriber.email,
         "Subject": "Din innloggingslink til Fotballvarsel",
         "TextBody": "Din innloggingslink til Fotballvarsel:\n" +
-            protocol + "://" + hostnameWithPort + "/#/loginByLink/"+ subscriber.email + "?secret=" + subscriber.secret +
-            "\n\n" + footer
+        loginLink(subscriber) + "\n\n" +
+        footer
     }, callback);
+};
+
+var loginLink = function(subscriber) {
+    return protocol + "://" + hostnameWithPort + "/#/loginByLink/"+ subscriber.email + "?secret=" + subscriber.secret;
 };
